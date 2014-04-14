251165dbfba9729f32a232131d2fd4b28d2c38cf
Diego Jornada <diego.p.jornada@gmail.com>
2014-04-13 21:31:42 -0300

    Organização nas chamadas do arquivo

--- main.c
+++ main.c
@@ -1,69 +1,85 @@
 #include "lcd.h"
 
+void set(int *arr,int index);
+int leds(int on, int blink);
+int verify(int *t, int *pswd);
+void init(void);
+void play(void);
+
+int main(){
+	int password[] = {0, 0, 0, 0};
+	int try[] = {0 ,0 ,0 ,0};
+
+	initTimer();
+
+	FIO4DIR = 0x0FF;
+	FIO4SET = 0xFF;
+
+	LCDinit();
+
+	LCDputs("##MASTER MIND!##");
+	espera(500);
+	LCDcomando(0xc0);
+
+	init();
+
+	LCDcomando(1);
+	LCDputs("SENHA SALVA");
+	espera(500);
+
+	if(play() < 10) LCDputs("You Win!");
+	else LCDputs("You Loose");
+
+	LCDcomando(1);
+	espera(500);
+	LCDcomando(1);
+
+	return 0;
+}
+
 void set(int *arr,int index){
 	if(arr[index] == 9) arr[index] = 0;
-
 	else arr[index] = arr[index]+1;
-
 	LCDcomando(0xC0 + (index+7));
 	LCDputchar('0' + arr[index]);
 	espera(200);
 }
+
 int leds(int on, int blink){
 	if(on == 4 ){ 
 		FIO4CLR = 0xF0;
 		return 1;
 	}
-	else if(on == 3){
-		FIO4CLR = 0xE0;
-	}
-	else if(on == 2){
-		FIO4CLR = 0xC0;
-	}
-	else if(on == 1){
-		FIO4CLR = 0x80;
-	}
+	else if(on == 3)	FIO4CLR = 0xE0;
+
+	else if(on == 2)	FIO4CLR = 0xC0;
+
+	else if(on == 1)	FIO4CLR = 0x80;
+
 	return 0;
 }
+
 int verify(int *t, int *pswd){
 	int right=0;
 	int wrong=0;
 	int i= 0;
-	for(i ; i<4 ; i++){
-		if(t[i] == pswd[i]) right++;
-	}
-	i = 0 ;
 	int aux = 0;
 	if(right < 4){
-	for(i ; i<4; i++){
-		int j=0;
-		for(j ; j<4;i++){
-			if(t[i] == pswd[j]){
-				aux++;
-				break;
+		for(i ; i<4; i++){
+			int j=0;
+			for(j ; j<4;i++){
+				if(t[i] == pswd[j]){
+					if(i==j) right++
+					else wrong++;
+					break;
+				}
 			}
 		}
 	}
-	wrong = aux - right;
-	}
 	return leds(right, wrong);
 }
-int main(){
-	int password[] = {0, 0, 0, 0};
 
-	int try[] = {0 ,0 ,0 ,0};
-
-	int *t = &try;
-	int lives = 0;
-
-	initTimer();
-
-	FIO4DIR = 0x0FF;		/* LED1 como saida */
-	FIO4SET = 0xFF;
-	LCDinit();
-	LCDputs("##MASTER MIND!##");
-	espera(500);
-	LCDcomando(0xc0);
+void init(void){
 	LCDputs("SENHA: 0000");
 	while(1){
 		if(!(FIO4PIN & 0x100)){
@@ -80,10 +96,9 @@ int main(){
 		} 
 		if(!(FIO4PIN & 0x1000)) break; 
 	}
-	LCDcomando(1);
-	LCDputs("SENHA SALVA");
-	espera(500);
-	LCDcomando(1);
+}
+int play(void){
+	int lives=0;
 	LCDputchar('#');
 	LCDcomando(0xC0);
 	LCDputs("       0000");
@@ -104,21 +119,12 @@ int main(){
 		} 
 		if(!(FIO4PIN & 0x1000)){
 			espera(300);
-			int  v = verify(t,&password);
-			if(v == 1) break;
-			
+			if(verify(t,&password) == 1) break;
 			lives++;
 		}  
 	}
-	espera(500);
-	LCDcomando(1);
-	LCDputs("FIM!!!");
-
-	espera(4000);
-	return 0;
+	return lives;
 }
-
-
 
 void UNDEF_Routine(){
 }
