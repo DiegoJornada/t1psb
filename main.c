1855f4149222bba231add1b361a3a952ecad6961
Diego Jornada <diego.p.jornada@gmail.com>
2014-04-22 13:16:56 -0300

    Firulas e Bug numeros repetidos ongoing

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-
-
 #include "lcd.h"
 int CONTROL = 0, RIGHT=0, WRONG=0;
 
@@ -10,142 +8,144 @@ void leds();
 int  play(int *pswd);
 
 int main(){
-		int password[] = {0, 0, 0, 0};
+	 int password[] = {0, 0, 0, 0};
 
-		initTimer();
+	 initTimer();
 
-		FIO4DIR = 0x0FF;
-		FIO4SET = 0xFF;
+	 FIO4DIR = 0x0FF;
+	 FIO4SET = 0xFF;
 
-		LCDinit();
+	 LCDinit();
 
-		LCDputs("##MASTER MIND!##");
+	 LCDputs("##MASTER MIND!##");
 
-		espera(500);
-		LCDcomando(0xc0);
+	 espera(500);
+	 LCDcomando(0xC0);
 
-		init(password);
+	 init(password);
 
-		LCDcomando(1);
-		LCDputs("SENHA SALVA");
-		espera(500);
-		LCDcomando(1);
+	 LCDcomando(1);
+	 LCDputs("##SENHA SALVA##");
+	 LCDcomando(0xC0);
+	 LCDputs("###############");
+	 espera(1000);
+	 LCDcomando(1);
 
+	 if(play(password) < 10) {
+			LCDcomando(1);
+			LCDputs("#Voce Ganhou!!!#");
+	 }else{
+			LCDcomando(1);
+			LCDputs("Voce Perdeu!:(");
+	 }
+	 LCDcomando(0xC0);
+	 LCDputs("##SW5:Reinicia##");
+	 while(1){ 
+			espera(100);
+			if(!(FIO4PIN & 0x1000)) break;
+	 }
 
-		if(play(password) < 10) {
-				LCDcomando(1);
-				LCDputs("#Voce Ganhou!!#");
-		}else{
-				LCDcomando(1);
-				LCDputs("Voce Perdeu!:(");
-		}
-
-		espera(4000);
-		return 0;
+	 return 0;
 }
 
 void set(int *arr,int index){
-		if(arr[index] == 9) arr[index] = 0;
+	 if(arr[index] == 9) arr[index] = 0;
 
-		else arr[index] = arr[index]+1;
+	 else arr[index] = arr[index]+1;
 
-		LCDcomando(0xC0 + (index+7));
-		LCDputchar('0' + arr[index]);
+	 LCDcomando(0xC0 + (index+7));
+	 LCDputchar('0' + arr[index]);
 }
 int ledAux(int n){
-		if(n == 4 )					return 0xF0;
+	 if(n == 4 )	return 0xF0;
 
-		else if(n == 3) return 0xE0;
+	 else if(n == 3) return 0xE0;
 
-		else if(n == 2) return 0xC0;
+	 else if(n == 2) return 0xC0;
 
-		else if(n == 1) return 0x80;
-		
-		else if(n == 0) return 0x00;
+	 else if(n == 1) return 0x80;
+
+	 else if(n == 0) return 0x00;
 
 }
 void leds(){
-		FIO4SET = 0xFF;
+	 FIO4SET = 0xFF;
 	 int on = ledAux(RIGHT);
-		int bl = 0x0;
-		if(CONTROL==0){
-			 bl = ledAux(WRONG)>>RIGHT;
-				CONTROL = 1;
-		}else{
-				CONTROL = 0;
-		}
-				FIO4CLR = on|bl;
-		/*
-		Se m==1				FIO4CLR = on | b; m = 0
-		Senão						FIO4CLR = on m =1;
-		*/
+	 int bl = 0x0;
+	 if(CONTROL==0){
+			bl = ledAux(WRONG)>>RIGHT;
+			CONTROL = 1;
+	 }else{
+			CONTROL = 0;
+	 }
+	 FIO4CLR = on|bl;
 }
 
 void verify(int *t, int *pswd){
-		int i= 0;
-		for(i ; i<4; i++){
-				int j=0;
-				for(j ; j<4;j++){
-						if(t[i] == pswd[j]){
-								if(i==j) RIGHT++;
+	 RIGHT = 0;
+	 WRONG = 0;
 
-								else WRONG++;
-
-								break;
-						}
-				}
-		}
+	 if(t[0] == pswd[0]) RIGHT++;
+	 if(t[1] == pswd[1]) RIGHT++;
+	 if(t[2] == pswd[2]) RIGHT++;
+	 if(t[3] == pswd[3]) RIGHT++;
+	 int aux = 0;
+	 int i= 0;
+	 for(i ; i<4; i++){
+			int j=0;
+			for(j ; j<4;j++){
+				 if(t[i] == pswd[j]){
+						aux++;
+						break;
+				 }
+			}
+	 }
+	 WRONG = aux - RIGHT; 
 }
 
 void init(int *pswd){
-		LCDputs("SENHA: 0000");
-		while(1){
-				if(!(FIO4PIN & 0x100)) set(pswd,0);
+	 LCDputs("SENHA: 0000");
+	 while(1){
+			if(!(FIO4PIN & 0x100)) set(pswd,0);
 
-				if(!(FIO4PIN & 0x200)) set(pswd,1); 
+			if(!(FIO4PIN & 0x200)) set(pswd,1); 
 
-				if(!(FIO4PIN & 0x400)) set(pswd,2); 
+			if(!(FIO4PIN & 0x400)) set(pswd,2); 
 
-				if(!(FIO4PIN & 0x800)) set(pswd,3);
+			if(!(FIO4PIN & 0x800)) set(pswd,3);
 
-				if(!(FIO4PIN & 0x1000)) break; 
-				espera(150);
-		}
+			if(!(FIO4PIN & 0x1000)) break; 
+			espera(150);
+	 }
 }
 
-/*
-			a=111000
-			b=000010
-			fio4pin=a|fio4pin^br
-	*/
-
 int play(int *pswd){
-		int try[] = {0,0,0,0};
-		int lives=0;
-		LCDputchar('#');
-		LCDcomando(0xC0);
-		LCDputs("       0000");
-		while(lives < 10){
-				LCDcomando(0x80 + 1);
-				LCDputchar('0' + lives);
-				if(!(FIO4PIN & 0x100)) set(try,0);	
+	 int try[] = {0,0,0,0};
+	 int lives=1;
+	 LCDputchar('#');
+	 LCDcomando(0xC0);
+	 LCDputs("       0000");
+	 while(lives < 11){
+			LCDcomando(0x80 + 1);
+			LCDputchar('0' + lives);
+			if(!(FIO4PIN & 0x100)) set(try,0);	
 
-				if(!(FIO4PIN & 0x200)) set(try,1); 
+			if(!(FIO4PIN & 0x200)) set(try,1); 
 
-				if(!(FIO4PIN & 0x400)) set(try,2);	
+			if(!(FIO4PIN & 0x400)) set(try,2);	
 
-				if(!(FIO4PIN & 0x800)) set(try,3);	
+			if(!(FIO4PIN & 0x800)) set(try,3);	
 
-				if(!(FIO4PIN & 0x1000)){
-						RIGHT = WRONG = 0;
-						verify(try, pswd);
-						if(RIGHT == 4) break;
-						lives++;
-				}  
-						espera(150);
-						leds();
-		}
-		return lives;
+			if(!(FIO4PIN & 0x1000)){
+				 verify(try, pswd);
+				 leds();
+				 if(RIGHT == 4) break;
+				 lives++;
+			}  
+			espera(150);
+			leds();
+	 }
+	 return lives;
 }
 
 void UNDEF_Routine(){
